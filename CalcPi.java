9dff8b9334adc5fdedaa12a9e2e1492128e6bb17
Bar Weitman <barweitman@gmail.com>
2024-11-15 15:55:21 +0200

    Updated

--- CalcPi.java
+++ CalcPi.java
@@ -3,6 +3,7 @@ public class CalcPi {
         int number = Integer.parseInt(args[0]);
         double addnum = 0;
         double total = 1;
+
         for (int i = 1; i < number; i++) {
             addnum = (double) 1 / (2 * i + 1);
             if (i % 2 == 0)
@@ -13,7 +14,13 @@ public class CalcPi {
         double appr = total * 4;
 
         System.out.println("pi according to Java: " + Math.PI);
-        
-        System.out.printf("pi, approximated:    %.15f\n", appr);
+
+        String approximatedPi = String.format("%.15f", appr).replaceAll("0*$", "");
+
+        if (approximatedPi.endsWith(".")) {
+            approximatedPi = approximatedPi + "0";
+        }
+
+        System.out.printf("pi, approximated:    %s\n", approximatedPi);
     }
 }
