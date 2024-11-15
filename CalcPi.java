42a546092ee2c6a878ddb5d6545db7b960b2a669
Bar Weitman <barweitman@gmail.com>
2024-11-15 15:46:25 +0200

    Final

--- CalcPi.java
+++ CalcPi.java
@@ -1,10 +1,8 @@
-// Computes an approximation of PI.
 public class CalcPi {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
         double addnum = 0;
         double total = 1;
-
         for (int i = 1; i < number; i++) {
             addnum = (double) 1 / (2 * i + 1);
             if (i % 2 == 0)
