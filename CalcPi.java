0d0915ecaada95ab2fc22b5911442ae2008d6611
Bar Weitman <barweitman@gmail.com>
2024-11-15 14:26:41 +0200

    HomeWork2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		double addnum = 0;
+		double total = 1;
+		for (int i = 1; i < number; i++){
+			addnum = (double) 1 / (2*i + 1);
+			if (i % 2 == 0)
+				total = (double) total + addnum;
+			else
+				total = (double) total - addnum;
+		}
+		double appr = total * 4;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + appr);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,23 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int number = Integer.parseInt(args[1]); 
+                String letter = "AEFHILMNORSX";
+                int num = word.length(); 
+                for (int give = 0; give < num; give++)
+                {
+                        String char1 = String.valueOf(word.charAt(give)); 
+                        if (letter.indexOf(char1) != -1) {
+                            System.out.println("Give me an " + char1 + ": " + char1 + "!");}
+                        else
+                                System.out.println("Give me a " + char1 + ": " + char1 + "!");
+                }
+                System.out.println("What does that spell?");
+                for (int i = 0; i < number; i++)
+                {
+                        System.out.println(word + "!!!");
+                }
         }
 }
+
